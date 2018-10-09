32613ba4367e4ea96e7546caf9267667bd997bf4
cassidyjclayton <cassidyjclayton@users.noreply.github.com>
2018-10-08 21:35:12 -0400

    test?
    attempting to add 'is-current' class to nav tag

--- student-info.php
+++ student-info.php
@@ -19,7 +19,7 @@
 			<h1>One MechE, Two DMS Majors</h1>
 		</header>
 
-		<nav class="navstyle">
+		<nav class="navstyle is-current">
 			<ul>
 				<li><a href="index.php">Homepage</a></li>
 				<li><a href="student-info.php">Students</a>
