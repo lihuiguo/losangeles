617a6ad1a0a94a2927f0a91a6fa9f125bbe00ba9
Aliye Gallagher <aliyegallagher@gmail.com>
2018-10-08 22:02:20 -0400

    added stylesheet to html docs

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 
 <head>
 	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/styles.css">
 	<link rel="stylesheet" href="css/styles_home.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Homepage</title>
